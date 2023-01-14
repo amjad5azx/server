84cf2c9774a5cf1083a45eff02feecf8e8ed3be2
Muhammad Amjad <muhammadamjad00@gmail.com>
2023-01-14 21:47:34 +0500

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const cookieParser=require("cookie-parser")
+const bcrypt=require('bcryptjs')
 require("./db/conn")
 const StudentRegister=require("./model/Student")
 
@@ -53,6 +54,48 @@ app.post("/studentRegister",async(req,res)=>{
 
 app.get("/studentLogin",(req,res)=>{
     res.send("")
+})
+
+app.post("/studentLogin",async (req,res)=>{
+    try {
+        const email=req.body.email
+        const pass=req.body.password
+        console.log("Email: "+email);
+        const useremail=await StudentRegister.findOne({email:email})
+        console.log("2) Email: "+email);
+        if(!useremail){
+            console.log("User not found");
+            return res.status(400).json({error:"User error"})
+            
+        }
+        console.log("3) Email: "+email);
+
+        const isMatch=await bcrypt.compare(pass,useremail.password)
+
+        console.log("3) Email: "+email);
+
+        const tokenA=await useremail.generateAuthToken()
+        console.log("the token part: 11 "+tokenA);
+
+        res.cookie("jwt", tokenA,{
+            expires:new Date(Date.now()+600000),
+            httpOnly:true
+            // secure:true
+        })
+
+        if(isMatch){
+            console.log("matched successfully");
+            res.send("Successful")
+        }
+        else{
+            console.log("Not Matched");
+            res.status(400).json({message:"User signin Failed"})
+        }
+    } catch (error) {
+        console.log("User Failed\n");
+        console.log(error);
+        return res.status(422).json({message:"User register failed"})
+    }
 })
 
 app.listen(port,()=>{
