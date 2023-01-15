15f413f6a7a01a132e981e47abe49e68e1e17ea4
amjad5azx <muhammadamjad00@gmail.com>
2023-01-15 19:48:19 +0500

    amjad

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const cookieParser=require("cookie-parser")
 const bcrypt=require('bcryptjs')
 require("./db/conn")
 const StudentRegister=require("./model/Student")
+const authenticate=require("./middleware/auth")
+const jwt=require("jsonwebtoken")
 
 const app=express()
 const  port=5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
 
 app.get("/studentRegister",(req,res)=>{
     res.send("send")
@@ -22,20 +25,52 @@ app.post("/studentRegister",async(req,res)=>{
     const pno=req.body.phoneNo
     const wno=req.body.whatsappNo
     const email=req.body.email
+    
+    if(!req.body.fname){
+        return res.json({message:"fname"})
+    }
+    if(!req.body.lname){
+        return res.json({message:"lname"})
+    }
+    if(!req.body.email){
+        return res.json({message:"emptyemail"})
+    }
+    if(!req.body.phoneNo){
+        return res.json({message:"emptyphone"})
+    }
+    if(!req.body.whatsappNo){
+        return res.json({message:"emptywhatsapp"})
+    }
+    if(!req.body.password){
+        return res.json({message:"epassword"})
+    }
+    if(!req.body.confirm_password){
+        return res.json({message:"ecpassword"})
+    }
 
     const useremail=await StudentRegister.findOne({email:email})
     const userphone=await StudentRegister.findOne({phoneNo:pno})
     const userwhatsapp=await StudentRegister.findOne({whatsappNo:wno})
 
+    if(userwhatsapp){
+        return res.json({message:"whatsapp"})
+    }
+    if(!req.body.whatsappNo){
+        return res.json({message:"emptywhatsapp"})
+    }
+    if(userphone){
+        return res.json({message:"phone"})
+    }
+    else if(!req.body.phoneNo){
+        return res.json({message:"emptyphone"})
+    }
     if(useremail){
         return res.json({message:"email"})
     }
-    else if(userphone){
-        return res.json({message:"phone"})
+    else if(!req.body.email){
+        return res.json({message:"emptyemail"})
     }
-    else if(userwhatsapp){
-        return res.json({message:"whatsapp"})
-    }
+    
 
     if(pass==cpass){
         const regStudent=new StudentRegister({
@@ -59,7 +94,7 @@ app.post("/studentRegister",async(req,res)=>{
 
     }
     else{
-        console.log("Password not matched");
+        return res.json({message:"password"})
     }
     } 
     catch (error) {
@@ -78,6 +113,10 @@ app.post("/studentLogin",async (req,res)=>{
     try {
         const email=req.body.email
         const pass=req.body.password
+
+        // if(!email||!pass){
+        //     return res.json({message:"empty"})
+        // }
         console.log("Email: "+email);
         const useremail=await StudentRegister.findOne({email:email})
         console.log("2) Email: "+email);
@@ -86,6 +125,13 @@ app.post("/studentLogin",async (req,res)=>{
             return res.status(400).json({error:"User error"})
             
         }
+
+        console.log("First Tokens: "+useremail.tokens);
+
+
+        useremail.tokens=[]
+        console.log("Empty Tokens: "+useremail.tokens);
+
         console.log("3) Email: "+email);
 
         const isMatch=await bcrypt.compare(pass,useremail.password)
@@ -112,7 +158,33 @@ app.post("/studentLogin",async (req,res)=>{
     } catch (error) {
         console.log("User Failed\n");
         console.log(error);
-        return res.status(422).json({message:"User register failed"})
+        return res.status(422).json({message:"User failed"})
+    }
+})
+
+app.get("/profile",authenticate,async (req,res)=>{
+    console.log("about");
+
+    const token=req.cookies.jwt
+        const verifyUSer=jwt.verify(token,"process.env.SECRET_KEY")
+
+    const user=await StudentRegister.findOne({_id:verifyUSer._id,"tokens.token":token})
+        console.log("Ye dekho");
+        console.log(user);
+
+    return res.json(user)
+})
+
+app.get("/logout",authenticate,async(req,res)=>{
+    try {
+        console.log(req.user);
+        req.user.tokens=[]
+        res.clearCookie("jwt")
+        await req.user.save()
+        console.log("logout successfully");
+        res.render("login")
+    } catch (e) {
+        res.status(500).send(e)
     }
 })
 
